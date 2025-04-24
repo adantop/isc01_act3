0b7f98326e65f4ac2a92146120c4965a0a2e248d
adantop <adn.lancer@gmail.com>
2025-04-24 16:08:29 -0600

    add utility

--- main.c
+++ main.c
@@ -4,28 +4,177 @@
 #include "types.h"
 #include "crypto.h"
 
+void gen_keys(int p, int q);
+
+PublicKey read_public_key(char *filename);
+
+PrivateKey read_private_key(char *filename);
+
+void write_data(char *filename, int count, int *crypt);
+
+int * read_data(char *filename, int *count);
+
+void print_help(char *cmd);
+
 int main(int argc, char *argv[argc]) {
+	if (argc != 4) {
+		print_help(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if (strcmp(argv[1], "genkeys") == 0) {
+		int p = atoi(argv[2]);
+		int q = atoi(argv[3]);
+		if (p * q < 127) {
+			fprintf(stderr, "p * q should be larger than 128\n");
+			return EXIT_FAILURE;
+		}
+
+		gen_keys(p, q);
+		return EXIT_SUCCESS;
+	} else if (strcmp(argv[1], "encrypt") == 0) {
+		PrivateKey priv = read_private_key(argv[2]);
+		int size = strlen(argv[3]);
+		int *c = encrypt(priv, argv[3], size);
+
+		// print the output
+		printf("[");
+		for (int i = 0; i < size - 1; i++) printf("%x, ", c[i]);
+		printf("%x]\n", c[size - 1]);
+
+		// Save the file
+		write_data("encrypted.bin", size, c);
+		return EXIT_SUCCESS;
+	} else if (strcmp(argv[1], "decrypt") == 0) {
+		PublicKey pub = read_public_key(argv[2]);
+		int size;
+		int *c = read_data(argv[3], &size);
+		
+		char *msg = decrypt(pub, c, size);
+		
+		printf("message: %s\n", msg);
+	} else {
+		fprintf(stderr, "invalid command\n");
+		print_help(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
+void gen_keys(int p, int q) {
 	// Cualquier combinacion de numeros primos cuya multiplicacion
 	// sea mayor que el numero de caracteres 128
-	Seed s = {2, 71};
+	// Seed s = {22531, 72997};
+	// Seed s = {2, 71};
+	Seed s = {p, q};
 	PrivateKey priv = mk_key(s);
 	PublicKey  pub  = mk_pub(priv);
 
-	printf("PrivateKey<e=%lld, n=%lld>\n", priv.e, priv.n);
-	printf("PublicKey<d=%lld, n=%lld>\n", pub.d, pub.n);
+	FILE *fp = fopen("key", "w");
+	if (!fp) {
+		perror("could not write private key");
+		return;
+	}
+	fprintf(fp, "%lld %lld %lld\n", priv.e, priv.d, priv.n);
+	fclose(fp);
 
-	char *m = "hello";
-	if (argc == 2) m = argv[1];
-	int size = strlen(m);
-	printf("Message size: %d\n", size);
-	int  *c = encrypt(priv, m, size);
-	char *n = decrypt(pub, c, size);
-
-	printf("\"%s\" --> \"", m);
-	for (int i = 0; i < size; i++) printf("%c", c[i]);
-	printf("\" [");
-	for (int i = 0; i < size - 1; i++) printf("%d, ", c[i]);
-	printf("%d] --> \"%s\"\n", c[size - 1], n);
-	
-	return EXIT_SUCCESS;
+	fp = fopen("key.pub", "w");
+	if (!fp) {
+		perror("could not write public key");
+		return;
+	}
+	fprintf(fp, "%lld %lld\n", pub.d, pub.n);
+	fclose(fp);
 }
+
+
+PublicKey read_public_key(char *filename) {
+	PublicKey *pub = malloc(sizeof(PublicKey));
+
+	FILE  *fp = fopen(filename, "r");
+	if (!fp) {
+		perror("could not read public key");
+		exit(EXIT_FAILURE);
+	}
+
+	fscanf(fp, "%lld %lld", &pub->d, &pub->n);
+	fclose(fp);
+	return *pub;
+}
+
+
+PrivateKey read_private_key(char *filename) {
+	PrivateKey *priv = malloc(sizeof(PrivateKey));
+
+	FILE  *fp = fopen(filename, "r");
+	if (!fp) {
+		perror("could not private public key");
+		exit(EXIT_FAILURE);
+	}
+
+	fscanf(fp, "%lld %lld %lld", &priv->e, &priv->d, &priv->n);
+	fclose(fp);
+	return *priv;
+}
+
+
+void write_data(char *filename, int count, int *crypt) {
+	FILE *fp = fopen(filename, "wb");
+	if (!fp) {
+		perror("could not save message");
+		exit(EXIT_FAILURE);
+	}
+
+	fwrite(crypt, sizeof(int), count, fp);
+	fclose(fp);
+}
+
+
+int * read_data(char *filename, int *count) {
+	FILE *fp = fopen(filename, "rb");
+	if (!fp) {
+		perror("could not open encrypted message file");
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+	rewind(fp);
+
+	*count = size / sizeof(int);
+
+	int *data = malloc(size);
+	if (!data) {
+		perror("could not load data");
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+
+	int read = fread(data, sizeof(int), *count, fp);
+	fclose(fp);
+
+	if (read != *count) {
+		fprintf(stderr, "read mismatch: expected %d, got %d\n", *count, read);
+		exit(EXIT_FAILURE);
+	}
+
+	return data;
+}
+
+
+void print_help(char *cmd) {
+	printf("usage: %s command args\n\n", cmd);
+	printf("commands:\n\n");
+	printf("    genkeys\n");
+	printf("      args: p q\n");
+	printf("      example: %s genkeys 2 71\n\n", cmd);
+	printf("    encrypt\n");
+	printf("      args: privkey message\n");
+	printf("      example: %s encrypt key \"hello world!\"\n\n", cmd);
+	printf("    decrypt\n");
+	printf("      args: pubkey file\n");
+	printf("      example: %s decrypt key.pub encrypted.bin\n", cmd);
+}
+
